ac610044d5aa092f18e3fa157e0ff5d1af62049a
Tor Henning Ueland <tor.henning@gmail.com>
2017-10-08 15:47:42 +0200

    Added data objects for search usage

---
+++ src/main/java/no/ueland/onionCrawler/objects/search/SearchDocument.java
@@ -0,0 +1,40 @@
+package no.ueland.onionCrawler.objects.search;
+
+public class SearchDocument {
+	private String URL;
+	private String hostname;
+	private String pageTitle;
+	private String pageContent;
+
+	public String getPageContent() {
+		return pageContent;
+	}
+
+	public void setPageContent(String pageContent) {
+		this.pageContent = pageContent;
+	}
+
+	public String getPageTitle() {
+		return pageTitle;
+	}
+
+	public void setPageTitle(String pageTitle) {
+		this.pageTitle = pageTitle;
+	}
+
+	public String getHostname() {
+		return hostname;
+	}
+
+	public void setHostname(String hostname) {
+		this.hostname = hostname;
+	}
+
+	public String getURL() {
+		return URL;
+	}
+
+	public void setURL(String URL) {
+		this.URL = URL;
+	}
+}
---
+++ src/main/java/no/ueland/onionCrawler/objects/search/SearchResult.java
@@ -0,0 +1,4 @@
+package no.ueland.onionCrawler.objects.search;
+
+public class SearchResult {
+}
