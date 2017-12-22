4e181122de1dedeb088e121e6dac88ad1376313e
Tor Henning Ueland <tor.henning@gmail.com>
2017-12-22 23:10:17 +0100

    Added total number of hits in data storage object

--- src/main/java/no/ueland/onionCrawler/objects/search/SearchResult.java
+++ src/main/java/no/ueland/onionCrawler/objects/search/SearchResult.java
@@ -1,4 +1,13 @@
 package no.ueland.onionCrawler.objects.search;
 
 public class SearchResult {
+	private int totalHits;
+
+	public int getTotalHits() {
+		return totalHits;
+	}
+
+	public void setTotalHits(int totalHits) {
+		this.totalHits = totalHits;
+	}
 }
